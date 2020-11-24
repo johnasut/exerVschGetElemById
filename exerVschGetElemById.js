3aca44005589cbde875ad7768272bcd77a762f69
John Saunders <john.saunders@pharm.utah.edu>
2020-11-23 19:05:06 -0700

    Get Element By ID Assignment

---
+++ exerVschGetElemById.js
@@ -0,0 +1,5 @@
+var newtext = document.getElementById("vschool");
+newtext.textContent = "Let's see if this works";
+
+var newertext = document.getElementById("apple");
+newertext.textContent = "Keeps the doctor away";
